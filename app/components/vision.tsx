4f794ef55cdd82d07abca735e5e1164038ad7830
Matias Onate <mationate.0@gmail.com>
2024-08-29 01:53:40 -0400

    fix deploy

--- app/components/vision.tsx
+++ app/components/vision.tsx
@@ -5,7 +5,7 @@ export const Vision = () => {
         <h3 className="text-lg md:text-xl text-gray-600 mb-2 text-center">Desbloquea tu potencial</h3>
         <h2 className="text-3xl md:text-5xl font-bold text-gray-900 mb-6 text-center">Domina el desafío.</h2>
         <p className="text-sm md:text-base text-gray-700 text-center leading-relaxed">
-          Con 'NOT A GAME' fomentamos la colaboración estratégica para enfrentar desafíos reales y complejos. A través de nuestra experiencia de juego, los participantes adquieren herramientas y habilidades que son esenciales para resolver problemas en una amplia gama de industrias. Este proceso no solo mejora su capacidad para enfrentar desafíos, sino que también los prepara para aplicar estos conocimientos de manera efectiva en su vida profesional, impulsando tanto su desarrollo personal como los resultados de las organizaciones.
+          Con NOT A GAME fomentamos la colaboración estratégica para enfrentar desafíos reales y complejos. A través de nuestra experiencia de juego, los participantes adquieren herramientas y habilidades que son esenciales para resolver problemas en una amplia gama de industrias. Este proceso no solo mejora su capacidad para enfrentar desafíos, sino que también los prepara para aplicar estos conocimientos de manera efectiva en su vida profesional, impulsando tanto su desarrollo personal como los resultados de las organizaciones.
         </p>
       </div>
       </div>
